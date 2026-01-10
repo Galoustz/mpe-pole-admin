5b864ef97651823def6a43c9d2c64f38e5183104
Galoustz <galousmgaya@gmail.com>
2026-01-10 18:09:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', () => {
         contributeBtn.textContent = 'Inasubiri...';
 
         try {
-            const response = await fetch('/api/fastlipa', {
+            const response = await fetch('/api/pay', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
